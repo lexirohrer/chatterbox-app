65a0c14b2d3371acc356fa7acf4dd1beab8fb085
Lexi Rohrer <lexirohrer@gmail.com>
2021-01-27 11:42:59 -0800

    made a text input box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import TextInput from './textinput';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [messages,setMessages] = useState([{text:'asdf'}])
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+    CHATTERBOX
+    </header>
 
+    <div className = "message">
+      {messages[0].text}
+    </div>
+
+    <TextInput
+      send={(t)=> setMessages([{text:t}])}
+    />
+      
+  </div>
+}
 export default App;
---
+++ src/textinput.js
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+
+function TextInput(props){
+    const [text, setText] = useState(' ')
+    
+    function sendMessage(){
+        if (text===''){return}
+        props.send(text)
+        setText(' ')
+    }
+
+    function keyPressed(e){
+        if (e.key==="Enter") {
+            sendMessage()
+        }
+    }
+    
+    return <footer className="text-input">
+    <input 
+    placeholder="write your message"
+    value={text}
+    onChange={e=> setText(e.target.value)}
+    onKeyPress= {keyPressed}
+    />
+    <button
+        onClick={sendMessage}
+    >
+        SEND</button>
+    </footer>
+
+}
+
+export default TextInput
